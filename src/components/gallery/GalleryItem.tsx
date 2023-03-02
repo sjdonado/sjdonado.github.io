3a88f34e11e6b87ca2c9b0fff07e29280dcfd708
Juan Rodriguez <sjdonado@uninorte.edu.co>
2023-03-02 06:50:34 +0100

    refactor: GalleryItem styles
    - fix width and pading
    - update footerMessage
    - add missing event

--- src/components/gallery/GalleryItem.tsx
+++ src/components/gallery/GalleryItem.tsx
@@ -11,9 +11,9 @@ interface GalleryProps {
 const GalleryItem: Component<GalleryProps> = (props) => (
   <div class="flex flex-col items-start w-72 m-1 rounded-lg border h-full self-start">
     <Img class="object-cover object-right-top  w-72 h-48 rounded-t-lg bg-gray-100" src={props.imageURL} alt={props.name} />
-    <div class="flex flex-col text-sm mx-2 my-1">
-      <span class="my-1 text-center border-b">{props.name}</span>
-      <span class="text-justify px-2">{props.description}</span>
+    <div class="flex flex-col w-full px-2 py-1">
+      <span class="text-sm text-center border-b">{props.name}</span>
+      <span class="text-xs text-justify">{props.description}</span>
     </div>
   </div>
 );
