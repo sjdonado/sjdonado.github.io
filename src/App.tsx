71b6d346662533b1ceffdf76edba93a7c62ec4ba
Juan Rodriguez <sjdonado@icloud.com>
2024-03-11 23:54:45 +0100

    feat: projects section

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import type { Component } from 'solid-js';
 
 import Header from './sections/Header';
 import Articles from './sections/Articles';
+import Projects from './sections/Projects';
 
 const App: Component = () => {
   return (
-    <div class="mx-auto my-12 flex h-dvh max-w-5xl flex-col gap-16 px-6">
+    <div class="mx-auto flex h-dvh max-w-5xl flex-col gap-16 px-6 py-12">
       <Header />
       <Articles />
+      <Projects />
     </div>
   );
 };
---
+++ src/sections/Projects.tsx
@@ -0,0 +1,35 @@
+import type { Component } from 'solid-js';
+
+import { projects } from '../data.json';
+
+const Projects: Component = () => {
+  return (
+    <section class="flex flex-col gap-8">
+      <h2 class="text-3xl font-semibold">{projects.title}</h2>
+      <div class="flex flex-row flex-wrap justify-center gap-4 lg:justify-start">
+        {projects.items.map(project => (
+          <div class="flex flex-row flex-wrap justify-center gap-4 rounded-lg border p-4">
+            <div class="avatar m-auto">
+              <div class="h-20 w-32 rounded-lg border">
+                <img src={project.image} alt={project.title} />
+              </div>
+            </div>
+            <div class="flex max-w-[300px] flex-col justify-center gap-2">
+              <h3 class="text-xl font-semibold">
+                <a href={project.link} class="link" target="_blank">
+                  {project.title}
+                </a>
+              </h3>
+              <p class="line-clamp-4 text-sm text-gray-500">{project.description}</p>
+              <a class="link text-sm" href={project.source} target="_blank">
+                Source code
+              </a>
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default Projects;
