bdc178af754575fab16c63f436d73dbd327391b3
Juan Rodriguez <sjdonado@icloud.com>
2024-03-12 14:03:13 +0100

    feat: slides section timeline

--- src/sections/Slides.tsx
+++ src/sections/Slides.tsx
@@ -5,22 +5,40 @@ import { slides } from '../data.json';
 const Slides: Component = () => {
   return (
     <section class="flex flex-col gap-8">
-      <ul class="list-inside">
+      <ul class="timeline timeline-vertical timeline-compact timeline-snap-icon">
         <For each={slides.items}>
           {slide => (
-            <li class="flex flex-col justify-center gap-1 p-4">
-              <span class="text-xs text-gray-500">{slide.date}</span>
-              <h3 class="text-xl font-semibold">
-                <a
-                  href={slide.link}
-                  class="link"
-                  target="_blank"
-                  rel="noopener noreferrer"
+            <li>
+              <hr />
+              <div class="timeline-middle">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                  class="size-5"
                 >
-                  {slide.title}
-                </a>
-              </h3>
-              <p class="line-clamp-4 text-sm">{slide.summary}</p>
+                  <path
+                    fill-rule="evenodd"
+                    clip-rule="evenodd"
+                    d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                  />
+                </svg>
+              </div>
+              <div class="timeline-end mb-8">
+                <time class="text-xs text-gray-500">{slide.date}</time>
+                <h3 class="text-xl font-semibold">
+                  <a
+                    href={slide.link}
+                    class="link"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    {slide.title}
+                  </a>
+                </h3>
+                <p class="line-clamp-4 text-sm">{slide.summary}</p>
+              </div>
+              <hr />
             </li>
           )}
         </For>
