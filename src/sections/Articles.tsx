992b1f1b2089a25d7ca64f62e9dc1376b87ebbe2
Juan Rodriguez <sjdonado@icloud.com>
2024-04-18 09:20:10 +0200

    fix: Projects + Articles long titles

--- src/sections/Articles.tsx
+++ src/sections/Articles.tsx
@@ -4,35 +4,33 @@ import type { ArticleItem } from '../@types';
 
 const Articles: Component<{ items: ArticleItem[] }> = ({ items }) => {
   return (
-    <section class="flex flex-col gap-8 lg:min-h-[29rem]">
-      <div class="flex flex-row flex-wrap justify-start gap-4 lg:justify-center">
-        <For each={items}>
-          {article => (
-            <div class="flex flex-col flex-wrap justify-center rounded-lg border border-base-content/20 p-4">
-              <span class="text-xs text-gray-500">{article.date}</span>
-              <h3 class="text-xl font-semibold">{article.title}</h3>
-              <div class="mt-4 flex flex-row gap-4">
-                <div class="avatar mx-auto pt-1">
-                  <div class="h-16 rounded-lg">
-                    <img src={article.image} alt={article.title} class="bg-gray-200" />
-                  </div>
-                </div>
-                <div class="flex max-w-[340px] flex-col justify-center gap-1">
-                  <p class="line-clamp-4 text-sm text-gray-500">{article.description}</p>
-                  <a
-                    class="link text-sm"
-                    href={article.link}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    Read article
-                  </a>
+    <section class="flex flex-row flex-wrap justify-center gap-4">
+      <For each={items}>
+        {article => (
+          <div class="flex max-w-md flex-col flex-wrap justify-start rounded-lg border border-base-content/20 p-4">
+            <span class="text-xs text-gray-500">{article.date}</span>
+            <h3 class="text-xl font-semibold">{article.title}</h3>
+            <div class="mt-4 flex flex-row gap-4">
+              <div class="avatar mx-auto pt-1">
+                <div class="size-16 rounded-lg">
+                  <img src={article.image} alt={article.title} class="bg-gray-200" />
                 </div>
               </div>
+              <div class="flex max-w-[340px] flex-col justify-center gap-1">
+                <p class="line-clamp-4 text-sm text-gray-500">{article.description}</p>
+                <a
+                  class="link text-sm"
+                  href={article.link}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Read article
+                </a>
+              </div>
             </div>
-          )}
-        </For>
-      </div>
+          </div>
+        )}
+      </For>
     </section>
   );
 };
--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -4,36 +4,34 @@ import { ProjectItem } from '../@types';
 
 const Projects: Component<{ items: ProjectItem[] }> = ({ items }) => {
   return (
-    <section class="flex flex-col gap-8">
-      <div class="flex flex-row flex-wrap justify-center gap-4 lg:justify-start">
-        <For each={items}>
-          {project => (
-            <div class="flex flex-row flex-wrap justify-center gap-4 rounded-lg border border-base-content/20 p-4">
-              <div class="avatar m-auto">
-                <div class="h-32 w-64 rounded-lg border sm:h-20 sm:w-32">
-                  <img src={project.image} alt={project.title} class="bg-gray-200" />
-                </div>
-              </div>
-              <div class="flex flex-col justify-center gap-2 sm:max-w-[300px]">
-                <h3 class="text-xl font-semibold">
-                  <a
-                    href={project.link}
-                    class="link"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    {project.title}
-                  </a>
-                </h3>
-                <p class="line-clamp-4 text-sm text-gray-500">{project.description}</p>
-                <a class="link text-sm" href={project.source} target="_blank">
-                  Source code
-                </a>
+    <section class="flex flex-row flex-wrap justify-center gap-4 lg:justify-start">
+      <For each={items}>
+        {project => (
+          <div class="flex flex-wrap justify-center gap-4 rounded-lg border border-base-content/20 p-4">
+            <div class="avatar m-auto">
+              <div class="h-32 w-64 rounded-lg border sm:h-20 sm:w-32">
+                <img src={project.image} alt={project.title} class="bg-gray-200" />
               </div>
             </div>
-          )}
-        </For>
-      </div>
+            <div class="flex flex-col justify-center gap-2 sm:max-w-72">
+              <h3 class="text-xl font-semibold">
+                <a
+                  href={project.link}
+                  class="link"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  {project.title}
+                </a>
+              </h3>
+              <p class="line-clamp-4 text-sm text-gray-500">{project.description}</p>
+              <a class="link text-sm" href={project.source} target="_blank">
+                Source code
+              </a>
+            </div>
+          </div>
+        )}
+      </For>
     </section>
   );
 };
