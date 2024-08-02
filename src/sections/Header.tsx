d67f6e49960d9b386a55d3aa29a65187919be3a8
Juan Rodriguez <sjdonado@icloud.com>
2024-08-02 18:49:42 +0200

    refactor: header horizontal alignment

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -9,28 +9,26 @@ export default function Header(props: { header: Header }) {
   });
 
   return (
-    <section class="flex flex-col justify-center gap-4 md:justify-center">
-      <div class="m-auto flex flex-col">
-        <div class="badge badge-outline">{props.header.subtitle}</div>
-        <h1 class="text-3xl font-bold md:text-4xl">{props.header.title}</h1>
-        <h2 class="text-gray-500">{props.header.username}</h2>
-        <div class="mt-2 flex flex-row gap-4">
-          <For each={props.header.socialMedia}>
-            {socialMedia => (
-              <a
-                class="link"
-                href={socialMedia.link}
-                target="_blank"
-                rel="noopener noreferrer"
-                aria-label={socialMedia.icon}
-              >
-                <i data-lucide={socialMedia.icon} class="size-6" />
-              </a>
-            )}
-          </For>
-        </div>
+    <section class="m-auto flex flex-col justify-center md:max-w-3xl md:justify-center">
+      <div class="badge badge-outline">{props.header.subtitle}</div>
+      <h1 class="text-2xl font-bold md:text-4xl">{props.header.title}</h1>
+      <h2 class="text-gray-500">{props.header.username}</h2>
+      <p class="my-4 text-justify text-sm md:text-base">{props.header.bio}</p>
+      <div class="mt-2 flex flex-row gap-4">
+        <For each={props.header.socialMedia}>
+          {socialMedia => (
+            <a
+              class="link"
+              href={socialMedia.link}
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label={socialMedia.icon}
+            >
+              <i data-lucide={socialMedia.icon} class="size-6" />
+            </a>
+          )}
+        </For>
       </div>
-      <p class="text-justify text-sm md:text-base">{props.header.bio}</p>
     </section>
   );
 }
