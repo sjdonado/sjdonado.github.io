92d499494600d6d9f21a773df268d53caaa34c2d
Juan Rodriguez <sjdonado@icloud.com>
2024-09-14 16:11:33 +0200

    refactor: Projects card styles

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -2,7 +2,7 @@ import type { Data } from '../schemas';
 
 export default function Header(props: { header: Data['header'] }) {
   return (
-    <section class="m-auto mt-4 flex flex-col justify-center md:max-w-3xl md:justify-center">
+    <section class="m-auto mt-4 flex flex-col justify-center md:max-w-2xl md:justify-center">
       <h1 class="text-4xl font-bold">{props.header.title}</h1>
       <h2 class="text-gray-500">{props.header.username}</h2>
       <p class="mt-2 text-justify text-sm md:text-base">{props.header.bio}</p>
--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -1,21 +1,20 @@
 import { For } from 'solid-js';
-
 import { Data } from '../schemas';
 
 export default function Projects(props: {
   items: Data['sections']['projects']['items'];
 }) {
   return (
-    <section class="flex flex-row flex-wrap items-center justify-center gap-4 lg:justify-start">
+    <section class="flex flex-row flex-wrap justify-center gap-2 lg:justify-start">
       <For each={props.items}>
         {project => (
-          <div class="mx-auto flex flex-wrap gap-4 rounded-lg border border-base-content/20 p-4">
-            <div class="avatar m-auto">
-              <div class="h-32 w-64 rounded-lg border sm:h-20 sm:w-32">
+          <div class="mx-auto flex flex-wrap justify-center rounded-lg border border-base-content/20">
+            <div class="avatar">
+              <div class="mt-4 size-32 rounded-lg border sm:mt-0 sm:h-full sm:w-40 sm:rounded-r-none sm:border-none">
                 <img src={project.image} alt={project.title} class="bg-gray-200" />
               </div>
             </div>
-            <div class="flex flex-col justify-center gap-2 sm:max-w-56">
+            <div class="flex flex-col justify-center gap-2 p-4 sm:max-w-64">
               <h3 class="text-xl font-semibold">
                 {project.link ? (
                   <a
@@ -32,7 +31,11 @@ export default function Projects(props: {
               </h3>
               <p class="line-clamp-4 text-sm text-gray-500">{project.description}</p>
               {project.source && (
-                <a class="link text-sm" href={project.source} target="_blank">
+                <a
+                  class="link text-xs font-semibold"
+                  href={project.source}
+                  target="_blank"
+                >
                   Source code
                 </a>
               )}
